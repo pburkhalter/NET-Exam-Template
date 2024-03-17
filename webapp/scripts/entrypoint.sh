52ec1fa3dbb48870cdaf31cac8b8d2ec3e5fb6c7
Patrik Burkhalter <20504120+pburkhalter@users.noreply.github.com>
2024-03-17 13:02:53 +0100

    WIP...

--- webapp/scripts/entrypoint.sh
+++ webapp/scripts/entrypoint.sh
@@ -6,6 +6,8 @@ export APP_DB_PASS=${APP_DB_PASS}
 export APP_DB_USER=${APP_DB_USER}
 export APP_DB_HOST=${APP_DB_HOST}
 
-
 # Start your Node.js application
-exec node index.js
+echo "$(date '+%Y-%m-%d %H:%M:%S') Starting webapp..."
+
+# Run the 'start' script from package.json
+exec npm start
